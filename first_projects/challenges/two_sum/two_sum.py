649d124ddca9f5116580762bf19b71fe0b9fecb6
github-bdub <brandonwhitey13@gmail.com>
2026-08-18 19:57:56 -0600

    Add initial setup for two-sum problem

--- first_projects/challenges/two_sum/two_sum.py
+++ first_projects/challenges/two_sum/two_sum.py
@@ -1,1 +1,10 @@
 print("Hello World! This is the updated message!")
+print("We are going to solve the two-sum problem:")
+
+nums = [1, 2, 3, 4, 5]
+target = 8
+output = []
+
+print("Here we go!")
+
+
